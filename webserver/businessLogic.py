debeb8183f1e6c6bb5ba4b77b1cfde502da32915
Johannes <muellerjohannes93@gmail.com>
2023-08-11 09:55:36 +0200

    Update businessLogic.py

--- webserver/businessLogic.py
+++ webserver/businessLogic.py
@@ -5,16 +5,17 @@ import whisper
 from pytube import YouTube
 
 
-def transcribeVideoOrchestrator(youtube_url: str, language: str):
+def transcribeVideoOrchestrator(youtube_url: str,  model_name: str):
     video = downloadYoutubeVideo(youtube_url)
-    transcription = transcribe(video)
+    transcription = transcribe(video, model_name)
     return transcription
 
 
-def transcribe(video: dict):
+def transcribe(video: dict, model_name="medium"):
     print("Transcribing...", video['name'])
-    model = whisper.load_model("medium")
-    result = model.transcribe(video['path'])
+    print("Using model:", model_name)
+    model = whisper.load_model(model_name)
+    result = model.transcribe(video['path'], )
     pprint(result)
     return result["text"]
 
